780b075d1bc7d514d19b29be4367aee9e85c9b7e
kasirabbas <kabbas24@gmail.com>
2019-07-04 09:20:39 +0100

    Update ItemController.cs
    Changed HttpPost to HttpGet

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -26,7 +26,7 @@ namespace WishList.Controllers
             return View("Create");
         }
 
-        [HttpPost]
+        [HttpGet]
         public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
